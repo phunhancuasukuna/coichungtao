6b798514f3de6e5c926be89dc4366b5173eaa620
Yen Tran <yentran@Yens-MacBook-Air.local>
2025-07-23 23:00:49 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Để cho cánh tay anh ôm em vào lònggg",
+    introTitle: 'Võ Trương Thiên Kỳ',
+    introDesc: `Em hay giấu tình cảm sau đôi ba câu đá xéo
+    Nhưng thật ra em yêu anh 
+    yêu đến rỉ máu từng đêm (thiệt đó ><)
+    Nên nếu phải làm tổn thương em thì hãy nhẹ nhàng 1 chút`,
+    btnIntro: '₊˚ʚ 🍓ɞ˚₊',
+    title: 'Lời tỏ tình cuối cùng của em˚🕊️ྀི ⋆⭒˚.⋆',
+    desc: 'Anh sẽ trả tự do cho trái tim em hay giữ e bên cạnh anh?? :333',
+    btnYes: 'YES! a đồng ý làm ngiuuu em <333',
+    btnNo: 'Không nha :3',
+    question: 'Tôi không hề ép bạn chọn yes đâU nha 😏 ',
+    btnReply: 'Gửi cho anh <3',
+    reply: '',
+    mess: 'Em biết mà 🥰. Yêu con nhà họ Võ :333',
+    messDesc: 'suốt cuộc đời này e ko quên chân tình dành hết cho anh <3',
+    btnAccept: 'anh cũng yêuuu em 💍',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/9112413958845207'
+}
